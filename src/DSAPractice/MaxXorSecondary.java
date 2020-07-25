b05e0114da9380a7d865898dc46cb126ada81702
Mustansir Kapasi <mustankap@gmail.com>
2020-07-26 01:23:03 +0530

    update

---
+++ src/DSAPractice/MaxXorSecondary.java
@@ -0,0 +1,19 @@
+package DSAPractice;
+
+import java.util.Scanner;
+
+public class MaxXorSecondary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			int n=sc.nextInt();
+			int s[]=new int[n];
+			
+			for(int i =0;i<n;i++)
+				s[i]=sc.nextInt();
+			
+			
+	}
+
+}
