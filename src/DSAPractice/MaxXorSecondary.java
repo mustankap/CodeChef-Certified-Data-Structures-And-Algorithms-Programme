8abd9c69a32c0ad2a552ec524bad3c80e629055c
Mustansir Kapasi <mustankap@gmail.com>
2020-07-31 01:32:42 +0530

    update

--- src/DSAPractice/MaxXorSecondary.java
+++ src/DSAPractice/MaxXorSecondary.java
@@ -1,5 +1,6 @@
 package DSAPractice;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MaxXorSecondary {
@@ -13,7 +14,31 @@ public class MaxXorSecondary {
 			for(int i =0;i<n;i++)
 				s[i]=sc.nextInt();
 			
-			
+			sub(s);
+	}
+	public static void sub(int arr[])
+	{
+		ArrayList<Integer> subset=new ArrayList<Integer>();
+			      
+			    
+			    int index = 0;
+			    subsetsUtil(arr, subset, index)  
+	}
+	public static void subsetsUtil(int[] A,ArrayList<Integer> subset,int index)
+	{
+		 
+		    System.out.print(subset);
+		    for(int i=index;i<A.length;i++)  
+		    {   
+		        
+		        subset.add(A[i]);
+		          
+		       
+		        subsetsUtil(A, subset, i + 1); 
+		          
+		        
+		        subset.pop(-1)  
+		    return;
 	}
 
 }
