869b45575c34d7961caeb55240ba0c28ead62685
Mustansir Kapasi <44290089+mustankap@users.noreply.github.com>
2020-08-26 01:22:30 +0530

    Create Enormous_Input_Test.java

---
+++ codechef_problems/Enormous_Input_Test.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+//https://www.codechef.com/problems/INTEST
+// Remember that the class name should be "Main" and should be "public".
+public class Enormous_Input_Test {
+	public static void main(String[] args) 
+	{
+	    try{
+	        
+	    
+		// System.in and System.out are input and output streams, respectively.
+	Scanner sc = new Scanner(System.in);
+
+	
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int ans = 0;
+		
+		for (int i = 0; i < n; i++) {
+			int x = sc.nextInt();
+			
+			if (x % k == 0) {
+				ans++;
+			}
+		}
+
+		System.out.println(ans);
+	} catch(Exception e)
+	{
+	    return;
+	}
+	}
+
+	
+}
