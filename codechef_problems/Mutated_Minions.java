f3269c682fb9fefb4ebec9ce857ec74d803acd59
Mustansir Kapasi <44290089+mustankap@users.noreply.github.com>
2020-08-26 01:29:33 +0530

    Create Mutated_Minions.java

---
+++ codechef_problems/Mutated_Minions.java
@@ -0,0 +1,45 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+//https://www.codechef.com/problems/CHN15A/
+/* Name of the class has to be "Main" only if the class is public. */
+class Mutated_Minions
+{
+	public static void main (String[] args) 
+	{ try {
+	    
+		    Scanner sc = new Scanner(System.in);
+		//System.out.println("ENTER NO OF TESTCASES:");
+		int t = sc.nextInt();
+		while(t--!=0)
+		{
+		    
+		//System.out.println("ENTER NO elements:");
+		int n = sc.nextInt();
+		//System.out.println("ENTER k value to add:");
+		int k = sc.nextInt();
+		//System.out.println("ENTER the elements:");
+		int a[] = new int[n];
+		int i,sum=0;
+		for(i=0;i<n;i++)
+		{
+		    a[i]=sc.nextInt();
+		}
+		
+		for(i=0;i<n;i++)
+		{
+		    a[i]+=k;
+		    if(a[i]%7==0)
+		    sum++;
+		}
+		System.out.println(sum);
+		
+	}
+	} catch(Exception e)
+	{return;
+	}
+	}
+}
+
