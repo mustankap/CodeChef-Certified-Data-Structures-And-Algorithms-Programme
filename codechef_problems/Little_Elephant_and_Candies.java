dd14600c504ed0df629aef31f4fb23d4b7a4d26b
Mustansir Kapasi <44290089+mustankap@users.noreply.github.com>
2020-08-26 01:26:01 +0530

    Create Little_Elephant_and_Candies.java

---
+++ codechef_problems/Little_Elephant_and_Candies.java
@@ -0,0 +1,55 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+//https://www.codechef.com/problems/LECANDY
+/* Name of the class has to be "Main" only if the class is public. */
+class Little_Elephant_and_Candies
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		try {
+		    Scanner sc = new Scanner(System.in);
+	  
+	  int t = sc.nextInt();
+	  while(t!=0)
+		{ 
+	          int n = sc.nextInt();
+	          int at[] = new int[n];
+		  
+	          int c = sc.nextInt(); int i;
+		  
+		  int m=0;
+		  for(i=0;i<n;i++)
+			{at[i]=sc.nextInt();
+			}
+                
+                 for(i=0;i<n;i++)
+			{ if(c>=at[i])
+				{
+               c-=at[i];
+				m++;}
+			 else{m=0;break;}
+			}
+                    if(m==0)
+                  {System.out.println("No");}
+		  else
+                 {System.out.println("Yes");
+                 }
+                   
+		 t--;
+		}
+	
+	
+
+
+
+
+		    
+		} catch(Exception e) {return;
+		} 
+		
+	}
+}
+
