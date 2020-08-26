4540704da97b106f9b3c0293f28132dec871cd70
Mustansir Kapasi <44290089+mustankap@users.noreply.github.com>
2020-08-27 00:16:17 +0530

    Update and rename Chef_and_Dynamo.java to Chef_and_Dynamo_DYNAMO.java

--- codechef_problems/Chef_and_Dynamo_DYNAMO.java
+++ codechef_problems/Chef_and_Dynamo_DYNAMO.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 //https://www.codechef.com/problems/DYNAMO/
 /* Name of the class has to be "Main" only if the class is public. */
-class Chef_and_Dynamo
+class Chef_and_Dynamo_DYNAMO
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
