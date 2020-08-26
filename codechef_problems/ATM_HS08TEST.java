25b3947e5c3beaac0f5b296de1a3b24679d1085e
Mustansir Kapasi <44290089+mustankap@users.noreply.github.com>
2020-08-27 00:15:29 +0530

    Update and rename ATM.java to ATM_HS08TEST.java

--- codechef_problems/ATM_HS08TEST.java
+++ codechef_problems/ATM_HS08TEST.java
@@ -3,7 +3,7 @@ import java.lang.*;
 import java.io.*;
 //https://www.codechef.com/problems/HS08TEST
 /* Name of the class has to be "Main" only if the class is public. */
-class ATM
+class ATM_HS08TEST
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
