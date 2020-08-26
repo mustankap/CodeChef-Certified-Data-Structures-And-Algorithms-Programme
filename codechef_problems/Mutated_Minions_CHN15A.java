c8f87ad0021c31f3d70dd6a87655d97a242f0dfd
Mustansir Kapasi <44290089+mustankap@users.noreply.github.com>
2020-08-27 00:19:21 +0530

    Update and rename Mutated_Minions.java to Mutated_Minions_CHN15A.java

--- codechef_problems/Mutated_Minions_CHN15A.java
+++ codechef_problems/Mutated_Minions_CHN15A.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 //https://www.codechef.com/problems/CHN15A/
 /* Name of the class has to be "Main" only if the class is public. */
-class Mutated_Minions
+class Mutated_Minions_CHN15A
 {
 	public static void main (String[] args) 
 	{ try {
